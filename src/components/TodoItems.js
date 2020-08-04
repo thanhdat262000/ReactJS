1f0770da84ff8bb715d1ff78739d26f44bb6c22a
thanhdat262000 <49014572+thanhdat262000@users.noreply.github.com>
2020-08-05 00:13:15 +0700

    todo-component

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
 import './TodoItems.css';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
+import close from '../img/close.svg';
 var classNames = require('classnames');
 class TodoItem extends Component {
     render() {
@@ -8,9 +11,15 @@ class TodoItem extends Component {
             'TodoItem':true,
             'TodoItem-complete': this.props.state,
         })
+        let url = checkImg;
+        if (this.props.state) {
+            url = checkCompleteImg;
+        }
     return (
-        <div className={className}>
+        <div className={className} >
+            <img src={url} alt='check' width={24} onClick={this.props.onClick}/>
             <p>{this.props.title}</p>
+            <img className='close' src={close} alt='close' width={20} onClick={this.props.onClose} />
         </div>
     )
     }
